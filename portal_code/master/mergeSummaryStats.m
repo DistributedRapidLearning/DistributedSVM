4be5f88ecd643d680a484da92b60e921c9ec787f
Timo Deist <timo.deist@maastro.nl>
2018-01-16 13:36:23 +0100

    store summary stats for each site in instance struct; add patient count to summary stats

--- portal_code/master/mergeSummaryStats.m
+++ portal_code/master/mergeSummaryStats.m
@@ -22,4 +22,16 @@ end
 instance.globalMin = min(minMatrix,[],1);
 % determine global maximum irrespective of training/test sites
 instance.globalMax = max(maxMatrix,[],1);
+
+for i_siteIndices = 1:length(sites)
+    instance.min(i_siteIndices,:) = sites(i_siteIndices).min; % min value per column
+    instance.max(i_siteIndices,:) = sites(i_siteIndices).max; % max value per column
+    instance.meas(i_siteIndices,:) = sites(i_siteIndices).means; % mean value per column
+    instance.median(i_siteIndices,:) = sites(i_siteIndices).medians; % median value per column
+    instance.mode(i_siteIndices,:) = sites(i_siteIndices).modes; % mode value per column
+    instance.std(i_siteIndices,:) = sites(i_siteIndices).std; % standard deviation per column
+    instance.count(i_siteIndices,:) = sites(i_siteIndices).count; % number of available values per column
+    instance.missing(i_siteIndices,:) = sites(i_siteIndices).missing; % number of missing values per column
+    instance.patientCount(i_siteIndices,:) = sites(i_siteIndices).patientCount; % number of patients/rows
+end
 end
