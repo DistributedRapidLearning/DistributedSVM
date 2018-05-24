ad97ca2df3bc5f6a6239b97ac6d4ef32fe301362
Johan van Soest <johan.vansoest@maastro.nl>
2018-05-24 10:16:59 +0200

    Updated parseLiteral o include int and integer

--- portal_code/site/parseLiteral.m
+++ portal_code/site/parseLiteral.m
@@ -5,6 +5,8 @@ function [ value ] = parseLiteral( dataType, value )
     switch char(dataType)
         case 'http://www.w3.org/2001/XMLSchema#int'
             value = str2num(strrep(value, ',', '.'));
+        case 'http://www.w3.org/2001/XMLSchema#integer'
+            value = str2num(strrep(value, ',', '.'));
         case 'http://www.w3.org/2001/XMLSchema#double'
             value = str2double(strrep(value, ',', '.'));
         otherwise
